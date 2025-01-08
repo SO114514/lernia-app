33215d304f64c4af482d019ec29845e1fb3cd161
seijiono <159113637+SO114514@users.noreply.github.com>
2025-01-08 08:44:16 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,13 +1,15 @@
-@@ -1,40 +1,47 @@
 //
 // Using example from:
 // https://go.dev/doc/tutorial/web-service-gin
 //
 package main
+
 import (
     "net/http"
+
     "github.com/gin-gonic/gin"
 )
+
 // album represents data about a record album.
 type album struct {
     ID     string  `json:"id"`
@@ -15,6 +17,7 @@ type album struct {
     Artist string  `json:"artist"`
     Price  float64 `json:"price"`
 }
+
 // albums slice to seed record album data.
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -43,22 +46,27 @@ func getIndex(c *gin.Context) {
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
 }
+
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
     var newAlbum album
+
     // Call BindJSON to bind the received JSON to
     // newAlbum.
     if err := c.BindJSON(&newAlbum); err != nil {
         return
     }
+
     // Add the new album to the slice.
     albums = append(albums, newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
+
 // getAlbumByID locates the album whose ID value matches the id
 // parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
     id := c.Param("id")
+
     // Loop through the list of albums, looking for
     // an album whose ID value matches the parameter.
     for _, a := range albums {
